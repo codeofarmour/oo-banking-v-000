c4868ea2cc263416116151b61c1b2402bea280a6
Terri Riggle <codeofarmour@outlook.com>
2018-07-10 17:42:07 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -16,10 +16,11 @@ class BankAccount
     "Your balance is $#{balance}."
   end
   
+  def valid?
+    balance > 0 && status == "open"
+  end
+  
   def close_account
     self.status = "closed"
   end
-  
-  
-
 end
